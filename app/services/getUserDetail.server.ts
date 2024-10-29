3421ecfc6cc707b0380e70093703d404cde43a26
Karcee <karmat359@gmail.com>
2024-10-29 16:49:57 +0530

    create user

--- app/services/getUserDetail.server.ts
+++ app/services/getUserDetail.server.ts
@@ -1,16 +1,15 @@
-// import { getHeaders } from "~/lib/getHeaders.server";
 import { getHeaders } from "~/lib/getHeaders.server";
 import { getUserSession } from "./session.server";
 
 export const createUser = async (user, request) => {
   const API_URL = process.env.API_ENDPOINT as string;
-  const url = API_URL + "/api/v1/user/create";
+  const url = API_URL + "/users";
   try {
     const response = await fetch(url, {
       body: JSON.stringify({
-        email: user.email,
         name: user.name,
-        picture: user.picture,
+        username: user.name,
+        email: user.email,
       }),
       headers: await getHeaders(request),
       method: "POST",
