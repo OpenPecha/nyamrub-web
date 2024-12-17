517692c10b491081f4f993879823808d7090fee6
Karcee <karmat359@gmail.com>
2024-12-17 10:04:26 +0530

    showing total user count

--- app/routes/about/route.tsx
+++ app/routes/about/route.tsx
@@ -1,6 +1,13 @@
-import React from 'react'
 import About from './components/About'
+import { json, LoaderFunction } from '@remix-run/node';
+import getTotalUser from '~/utils/getTotaluser';
 
+export const loader: LoaderFunction = async ({ request }) => {
+    const url = process.env.API_ENDPOINT + "/get_all_users";
+  const totalUser = await getTotalUser(url);
+  console.log("total user ;:::: ",totalUser)
+  return json({ totalUser });
+}
 export default function route() {
   return (
       <div className='bg-primary-50 h-[calc(100vh-90px)]'>
