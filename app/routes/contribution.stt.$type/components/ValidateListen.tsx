1083185e27b29d8ac9f432aff4f5482be80ab7c1
Karcee <karmat359@gmail.com>
2024-12-18 13:17:25 +0530

    stt responsive

--- app/routes/contribution.stt.$type/components/ValidateListen.tsx
+++ app/routes/contribution.stt.$type/components/ValidateListen.tsx
@@ -4,6 +4,7 @@ import ActionBtn, { Correctbtn, Incorrectbtn, Skipbtn } from "~/components/Butto
 import ValidateMore from "~/components/ValidateMore";
 import AudioPlayer from "~/components/AudioPlayer";
 import CurrentStatus from "~/components/CurrentStatus";
+import Progressbar from "~/components/Progressbar";
 
 interface ListenValidation {
   validation_id: string;
@@ -20,7 +21,6 @@ export default function ValidateListen() {
   // Hooks
   const { data: listen_validations = [], user_id } =
     useLoaderData<LoaderData>();
-  console.log(listen_validations);
   const fetcher = useFetcher();
   const audioRef = useRef<HTMLAudioElement>(null);
 
@@ -94,9 +94,10 @@ export default function ValidateListen() {
 
   return (
     <div className="grid grid-cols-6 grid-rows-8 py-4 w-full h-full">
-      <div className="row-span-6" />
-      <div className="col-span-4 row-span-6 bg-white shadow-md rounded-3xl overflow-hidden">
-        <div className="flex flex-col justify-around items-center h-full py-5">
+      <div className="row-span-6 hidden md:block" />
+      <div className="col-span-6 md:col-span-4 row-span-7 md:row-span-6 bg-white shadow-md rounded-lg overflow-hidden mx-4">
+        <div className="flex flex-col justify-around items-center h-full py-5 px-2 bg-white shadow-md rounded-lg relative">
+                    <Progressbar totalNumbers={totalValidation} />
           <div className="flex items-center justify-center w-full">
             <div className="flex-1 text-lg font-semibold font-monlam text-center text-primary-900">
               སྒྲ་ཇི་བཞིན་ཡིག་འབེབ་བྱོས།
@@ -104,7 +105,7 @@ export default function ValidateListen() {
           </div>
           <AudioPlayer tempAudioURL={currentValidation?.source_audio_url} />
           <textarea
-            className="bg-neutral-50 rounded-lg resize-none text-neutral-950 font-monlam text-lg font-medium leading-loose focus:outline-none focus:ring-0 border placeholder:text-neutral-700 placeholder:text-xs placeholder:font-medium py-6 px-4 w-3/4  placeholder:font-monlam"
+            className="bg-neutral-50 rounded-lg resize-none text-neutral-950 font-monlam text-lg font-medium leading-loose focus:outline-none focus:ring-0 border placeholder:text-neutral-700 placeholder:text-xs placeholder:font-medium py-6 px-4 w-full md:w-3/4  placeholder:font-monlam"
             placeholder="འདིར་ཡི་གེ་འབྲི།"
             rows={5}
             value={currentValidation?.contribution_text}
@@ -112,16 +113,19 @@ export default function ValidateListen() {
           />
         </div>
       </div>
-      <div className="row-span-6 place-content-center">
+      <div className="row-span-6 place-content-center hidden md:block">
         <CurrentStatus totalNumbers={totalValidation} />
       </div>
       <div className="col-span-full place-content-center">
-        <div className="flex flex-row items-center justify-center h-full space-x-6">
+        <div className="flex flex-row items-center justify-center h-full space-x-2 md:space-x-6">
+          <div className="md:hidden">
+            <Skipbtn handleClick={handleSkip} />
+          </div>
           <Incorrectbtn handleClick={() => handleSubmit(false)} />
           <Correctbtn handleClick={() => handleSubmit(true)} />
         </div>
       </div>
-      <div className="col-span-full">
+      <div className="col-span-full hidden md:block">
         <div className="flex items-start justify-end h-full">
           <Skipbtn handleClick={handleSkip} />
         </div>
