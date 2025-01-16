369d440bef170e5bba84cddf377b8f3e28877d19
Karcee <karmat359@gmail.com>
2025-01-16 15:02:01 +0530

    add skeleton

--- app/routes/contribution.mt.$type/route.tsx
+++ app/routes/contribution.mt.$type/route.tsx
@@ -6,7 +6,6 @@ import ValidateSegment from "./components/ValidateSegmentText";
 import fetchData from "../../utils/fetchData";
 import { Suspense } from "react";
 import SkeletonFallback from "./components/SkeletonFallback";
-import LoadingSpinner from "~/components/LoadingSpinner";
 
 export const loader: LoaderFunction = async ({ request, params }) => {
   const API_ENDPOINT: string | undefined = process.env.API_ENDPOINT;
@@ -34,7 +33,7 @@ export default function route() {
   const { type } = useParams();
   const {data} = useLoaderData()
   return (
-    <Suspense fallback={<LoadingSpinner/>}>
+    <Suspense fallback={<SkeletonFallback />}>
       <Await resolve={data}>
         {(data) => (
           <div className="flex flex-col items-center w-full h-full">
